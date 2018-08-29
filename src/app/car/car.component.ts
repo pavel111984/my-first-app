696f189b9180822c5adf93935925cebf6332c79d
pavel111984 <pavel.mikhaylov84@gmail.com>
2018-08-30 00:33:39 +0300

    block3 components

---
+++ src/app/car/car.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-car',
+  templateUrl: './car.component.html',
+  styleUrls: ['./car.component.css']
+
+})
+export class CarComponent {
+@Input('carItem') car: { name: string, year: number};
+    /*getName() {
+        return this.carName;
+    }*/
+}
---
+++ src/app/cars/cars.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cars',
+  templateUrl: './cars.component.html',
+  styleUrls: ['./cars.component.css']
+})
+export class CarsComponent {
+
+    
+    carName = '';
+    carYear = 2017;
+    cars: {name: string, year: number}[] = [{ name: 'Ford', year: 2015 },{ name: 'Audi', year: 2010 },{ name: 'BMW', year: 2011 },{ name: 'Mazda', year: 2016 }];
+    //items = [{id:3, name: 'item1'},{id:6, name: 'item2'},{id:9, name: 'item3'}];
+   /* dates = [
+      new Date(2015, 3, 4).toDateString(),
+      new Date(2011, 2, 5).toDateString(),
+      new Date(2017, 4, 6).toDateString(),
+      new Date(2000, 1, 7).toDateString(),
+        
+    ];
+    */
+    constructor () {
+    }
+
+    addCar () {
+        
+        this.cars.push({name: this.carName, year: this.carYear});
+        this.carName = '';
+        this.carYear = 2017;
+    }
+    
+  
+}
